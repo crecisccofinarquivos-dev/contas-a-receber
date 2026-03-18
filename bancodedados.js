53784d75e5280c13eff1fd8c67d4def9f4cae343
crecisccofinarquivos-dev <crecisc.cofin.arquivos@gmail.com>
2026-03-18 09:46:05 -0300

    envio dos arquivos dia 18-03-26

---
+++ bancodedados.js
@@ -0,0 +1,136 @@
+﻿var bibliotecaInicial = [
+  {
+    "titulo": "01 Cancelamento PF",
+    "texto": "<div><p><b>Instruções para Cancelamento de Inscrição de Pessoa Física – CRECI-SC<br></b>Acesse o Portal de Atendimento Online pelo&nbsp; link:<br><a href=\"https://atendimento.crecisc.conselho.net.br/#/\">https://atendimento.crecisc.conselho.net.br/#/</a>.<br>1- Informe seu CPF.<br>2- Crie uma senha (não é necessário que seja a mesma utilizada no Recadastramento Obrigatório).<br>3- Clique em <strong>“Novo Requerimento”</strong>.<br>Selecione a categoria <strong>“REQUERIMENTOS PESSOA FÍSICA”</strong>.<br>Escolha a opção desejada:<br><strong>“Cancelamento da Inscrição de Pessoa Física”<br></strong><b>“Cancelamento da Inscrição por Falecimento”<br></b>Para informações adicionais sobre o processo, entre em contato com a Secretaria do<br>Conselho pelo e-mail: <b>secretaria@creci-sc.gov.br<br></b><strong>Observação:</strong> Conforme a Portaria nº 27, de 19 de janeiro de 2022, se o desligamento do<br>Conselho ocorrer <b>até 31 de março de cada ano</b>, o(a) corretor(a) <b>ficará isento(a) do<br>pagamento da anuidade daquele ano.</b> Após essa data, a anuidade será cobrada<br><b>proporcionalmente</b> aos meses em que a inscrição permaneceu ativa.<br>Para verificar sua situação financeira, estamos disponíveis pelo e-mail:<br><b>financeiro@crecisc.gov.br<br></b>Estamos à disposição para auxiliá-lo(a) em qualquer dúvida.</p></div>"
+  },
+  {
+    "titulo": "02 Solicitação de 2º via de boleto",
+    "texto": "<p>Olá, o CRECI-SC agradece o seu contato.<br><br>Encaminhamos, em anexo, a 2ª via do boleto. Solicitamos que, <b>antes de efetuar o pagamento, verifique se o nome, número do CRECI, CPF ou CNPJ estão corretos.<br><br></b>Tenha um ótimo dia.</p><p><br></p><p><br></p>"
+  },
+  {
+    "titulo": "03 Envio de Levantamento de Débitos",
+    "texto": "<p>Prezado(a),<br>Conforme verificação realizada, constam em aberto as anuidades referentes ao período de 20-- a 20--. O<b> valor total atualizado do débito</b> das anuidades é de R$---. Para <b>pagamento à vista,</b> com aplicação do<b> desconto máximo permitido pela legislação vigente</b>, o valor é reduzido para R$ ---.<br>Também é possível realizar o parcelamento em até -- parcelas, conforme condições estabelecidas, com incidência dos encargos correspondentes. Informamos que as anuidades referentes ao período de 20-- a 20-- <b>encontram-se inscritas no CADIN e em fase de execução judicial.</b> Em razão disso, incidem também:<br><b><br></b></p><p><b>Honorários advocatícios:</b> R$ ---<br><b>Custas judiciais: </b>R$ ---<br><br></p><p>Ressaltamos que os valores de honorários advocatícios e custas judiciais <b>NÃO</b> estão<br>incluídos no montante das anuidades, sendo cobrados <font color=\"#f50a0a\"><b>separadamente</b></font>.<br>Permanecemos à disposição para esclarecimentos e para encaminhar as opções de<br>pagamento.</p>"
+  },
+  {
+    "titulo": "04 Pedir dados ao corretor",
+    "texto": "<div>Olá, o CRECI-SC agradece o seu contato.<br><br>As informações apresentadas são insuficientes para que possamos localizar seu cadastro em nosso sistema, por conta disso, solicitamos que <b>informe seu nome completo, CPF ou CNPJ</b><br><b>e número do CRECI.<br><br></b>Tenha um ótimo dia.</div>"
+  },
+  {
+    "titulo": "05 Maratona Negocia Fácil",
+    "texto": "<div>Olá, o CRECI-SC agradece o seu contato.<br>Segue abaixo as possíveis condições de negociação conforme solicitado. Lembrando que todas as condições apresentadas serão válidas <b>apenas durante o período da maratona negocia fácil.<br><br></b>Tenha um excelente dia.&nbsp;</div>"
+  },
+  {
+    "titulo": "06 Quando o corretor Pede mais Negociação",
+    "texto": "<div><font size=\"3\" style=\"\">Olá, t</font><font size=\"3\" style=\"\">odas as condições possíveis de negociação foram apresentadas acima,<b>&nbsp;</b></font>elas seguem a<br>resolução <b>COFECI 1482/2022</b> e o ato interno <b style=\"\">16/2002</b><b>.<br><br></b>Obrigado, tenha um ótimo dia.&nbsp;</div>"
+  },
+  {
+    "titulo": "07 Boleto de Quitação",
+    "texto": "<div>Olá, segue em anexo o boleto para quitação conforme solicitado, <b>antes de efetuar o pagamento verifique se o nome, número do CRECI, CPF ou CNPJ estão corretos.<br></b>Tenha um ótimo dia, obrigado.&nbsp;</div>"
+  },
+  {
+    "titulo": "08 Envio de TCD",
+    "texto": "<div>Olá, segue Termo de Confissão de Dívida em anexo, pode ser realizada a impressão, assinar à<br>mão e encaminhar neste requerimento o documento escaneado para que possamos emitir<br>os boletos.<br>Foi enviado ao seu e-mail cadastrado as instruções para assinatura digital, caso prefira.<br>Também pode ser assinado via o aplicativo iCorretor, na aba Financeiro, no campo Acordos.&nbsp;<br><br><b>Obs: O código para a assinatura digital do TCD se encontra na</b><font color=\"#ff0505\" style=\"font-weight: bold;\"> 2º linha</font><b> abaixo da data.<br><br></b>Conforme Claúsula 5º do TCD a<b> Certidão de Regularidade somente ficará disponível após</b><br><b>o pagamento da 1º parcela do acordo.<br></b>Ressalto que, <b>na hipótese de ajuizamento de execução fiscal dos débitos</b> apurados<br>durante o período de negociação, <b>serão acrescidos honorários advocatícios no percentual<br>de 10% (dez por cento) sobre o valor atualizado do débito, caso a negociação não seja<br>formalizada por meio de TCD (Termo de Confissão de Dívida).</b></div>"
+  },
+  {
+    "titulo": "09 Envio de boletos após o recebimento do TCD",
+    "texto": "<div>Olá, grato pelo envio do TCD assinado. Segue anexo os boletos do parcelamento. Solicitamos que <font color=\"#030303\"><b>antes de efetuar o pagamento verifique se o nome, número do CRECI, CPF ou CNPJ</b><br><b>estão corretos.<br><br></b></font>Tenha um ótimo dia.&nbsp;</div><div><br></div>"
+  },
+  {
+    "titulo": "10 Quando o Corretor Fala que Nunca Trabalhou",
+    "texto": "<div>Olá, toda cobrança feita pelo CRECI-SC é sob <b>legislação</b>, deste modo, conforme <b>Art. 5º&nbsp;da Lei nº 12.514 de 28 de Outubro de 2011</b>, o<i><b> fato gerador</b> das anuidades é a <b>existência de<br>inscrição no Conselho</b>, ainda que por tempo limitado, ao longo do exercício.<br><br></i>Tenha um ótimo dia, obrigado.</div><div><br></div>"
+  },
+  {
+    "titulo": "11 Reembolso com Cancelamento em Andamento",
+    "texto": "<div>Olá, o CRECI-SC agradece o seu contato.</div><div>Ao verificar seu processo de cancelamento foi possível constatar que o mesmo <b>está em trâmite.&nbsp;</b>Solicito que <b>aguarde o processo de cancelamento ser concluído</b> e <b>após receber o ofício de cancelamento encaminhe o requerimento de reembolso novamente.&nbsp;</b>Infelizmente não é possível realizar a devolução até que o processo de cancelamento seja finalizado. No caso de o requerimento ser encerrado por inatividade, solicito por gentileza que abra uma nova solicitação.</div><div><br></div><div>Tenha um ótimo dia.</div><div><br></div>"
+  },
+  {
+    "titulo": "12 Envio de Boleto Protestado",
+    "texto": "<div>Segue anexo o boleto referente ao título protestado. <b>Pedimos, por gentileza, que verifique se o nome e o número do CPF estão corretos.</b> Após a quitação do boleto, solicitamos que envie o comprovante de pagamento para o endereço e-mail <b>financeiro@creci-sc.gov.br</b> informando nome completo e CPF para identificação.&nbsp;</div><div><b>Assim que confirmarmos o recebimento, encaminharemos, no prazo de até 48 horas, a documentação necessária para a baixa do protesto. Com esse documento em mãos, será preciso comparecer ao cartório para efetuar o pagamento das taxas cartorárias e concluir a baixa definitiva.</b></div>\n<p data-start=\"679\" data-end=\"731\">Ficamos à disposição para quaisquer esclarecimentos.</p>"
+  },
+  {
+    "titulo": "13 Envio de Certidão de Protesto",
+    "texto": "Olá, encaminhamos a autorização em anexo.<br>Pedimos, por gentileza, que o (a) senhor (a) entre em contato com o cartório para realizar o<br>pagamento das taxas correspondentes e requerer a baixa definitiva do protesto.<br><b>Observação:</b> <b>Caso o cartório questione a ausência de assinatura, orientamos informar<br>que o documento possui assinatura digital realizada pelo sistema.</b>"
+  },
+  {
+    "titulo": "14 Envio de Protocolo de Reembolso",
+    "texto": "<div><p class=\"isSelectedEnd\">Olá, o CRECI-SC agradece o seu contato. Encaminhamos o protocolo em anexo. <b>Informamos que o prazo para análise é de 30 a 120 dias.<br></b><b><br>Observação:</b> Seu chamado poderá ser encerrado por inatividade; contudo, não se preocupe, pois o processo seguirá normalmente. Após a conclusão e a efetivação da devolução, o comprovante será enviado para o e-mail cadastrado em nosso sistema.</p></div><div><br></div>"
+  },
+  {
+    "titulo": "15 Atualização Cadastral",
+    "texto": "<div><p>Olá, o CRECI-SC agradece o seu contato.<br>Para realizar alterações ou atualizações em seu cadastro, é necessário solicitar um novo requerimento por meio do Portal de Atendimento.<br>Acesse o link abaixo:<br><a href=\"https://atendimento.crecisc.conselho.net.br/\">https://atendimento.crecisc.conselho.net.br</a><br><br>Siga as orientações:<br>1 - Informe seu CPF;<br>2 - Crie uma senha (anote-a em local seguro);<br>3 - Clique em “Novo Requerimento”;<br>4 - Selecione a opção “ICORRETOR - Ajuste de Cadastro (opção 4)”.</p></div><div><br></div>"
+  },
+  {
+    "titulo": "16 Duvidas de Inscrição PF",
+    "texto": "<div>Olá, o CRECI-SC agradece o seu contato.<br>Seu processo será encaminhado para nossa Sede, em Florianópolis, onde <b>tramitará pelo<br>prazo médio de 25 a 35 dias após o pagamento das taxas.</b> Após a conclusão do trâmite,<br>entraremos em contato por meio do seu e-mail cadastrado para informar sobre o juramento<br>da profissão e a entrega da sua numeração CRECI.<br><b>Por gentileza, ao final do prazo de 25 a 35 dias, consulte com frequência seu e-mail,<br>bem como as pastas de spam e lixo eletrônico, para verificar o recebimento das<br>orientações.</b></div><div>&nbsp;<span style=\"white-space:pre\">\t\t\t</span></div><div><br></div>"
+  },
+  {
+    "titulo": "17 Informação Desarquivamento",
+    "texto": "<p class=\"isSelectedEnd\">Olá, o CRECI-SC agradece o seu contato.<br>Ao verificar seu cadastro, identificamos que o seu <b>processo foi arquivado.&nbsp;</b>Solicitamos, por gentileza, que entre em contato com o setor de Secretaria para requerer o desarquivamento, por meio do endereço de e-mail: <a href=\"mailto:secretaria@creci-sc.gov.br\">secretaria@creci-sc.gov.br</a>.<br><br>Tenha um ótimo dia.<br>Obrigado.</p>"
+  },
+  {
+    "titulo": "18 Caso questione a prescrição do débito",
+    "texto": "<div><p>Em resposta ao questionamento sobre a prescrição do crédito, cumpre esclarecer que o<br>art. 174 do CTN - Código Tributário Nacional deve ser interpretado à luz da Lei nº<br>12.514/2011, art. 8º, e do Tema 22 do TRF-4, aplicados ao caso concreto:<br><strong>Art. 8º</strong> – Os Conselhos não executarão judicialmente dívidas, de quaisquer das origens<br>previstas no art. 4º desta Lei, com valor total inferior a cinco vezes o constante do inciso I<br>do caput do art. 6º desta Lei, observado o disposto no § 1º.<br><strong>§ 1º</strong> – O disposto no caput não impede ou limita a adoção de medidas administrativas de<br>cobrança, como notificação extrajudicial, inclusão em cadastros de inadimplentes e<br>protesto de certidões de dívida ativa.<br>Conforme a tese fixada pelo Tema 22 do TRF-4, que trata da solução de demandas<br>repetitivas, aplica-se o entendimento de que:<br><br><i>\"Os Conselhos de fiscalização profissional não possuem interesse processual no ajuizamento<br>de protesto interruptivo da prescrição (CTN, art. 174, parágrafo único, II) enquanto não<br>reunidas as quatro anuidades a que se refere o art. 8º da Lei nº 12.514/11, porque durante<br>este período não se inicia o prazo prescricional para a propositura da execução fiscal.\"<br></i>Portanto, o prazo para prescrição do crédito terá início somente a partir da quinta<br>anuidade em atraso.</p></div><div><span style=\"white-space: pre; white-space: normal;\">\t\t</span></div><div><span style=\"white-space: pre; white-space: normal;\">\t\t</span></div><div><span style=\"white-space: pre; white-space: normal;\">\t\t</span></div>"
+  },
+  {
+    "titulo": "19 Desconto Anuidade",
+    "texto": "<p><font size=\"3\">Informamos que a anuidade vence anualmente no dia 31/03 e é gerada normalmente em<br>dezembro do ano anterior.<br></font><span style=\"font-size: medium;\">As condições de pagamento são as seguintes:<br></span><strong style=\"font-size: medium;\"><br>Até 15/01</strong><span style=\"font-size: medium;\">: desconto de 10% para pagamento à vista e possibilidade de parcelamento em até 5 vezes;<br></span><strong style=\"font-size: medium;\">Até 15/02</strong><span style=\"font-size: medium;\">: desconto de 6% para pagamento à vista e possibilidade de parcelamento em até 4 vezes;<br></span><strong style=\"font-size: medium;\">Até 15/03</strong><span style=\"font-size: medium;\">: desconto de 4% para pagamento à vista e possibilidade de parcelamento em até 3 vezes.</span></p>"
+  },
+  {
+    "titulo": "20 Texto Multa Desc. De Acordo",
+    "texto": "<p>Informamos que, devido ao não cumprimento do acordo firmado, foi aplicada uma multa por descumprimento, conforme previsto no Termo de Confissão de Dívida assinado na<br>formalização do acordo, <strong>item 3.2 C</strong>. O reparcelamento do débito não está autorizado no momento. Contudo, de acordo com a portaria vigente, após a quitação da multa pelo descumprimento, os débitos poderão ser reparcelados, nos termos da <strong>Resolução COFECI nº 1.482/2022</strong>.</p>"
+  },
+  {
+    "titulo": "21 Gravidez antes da Resolução",
+    "texto": "<p class=\"MsoNormal\">Olá, o CRECI-SC agradece o seu contato.<br>Após análise da documentação\nencaminhada, verificamos que o nascimento ocorreu em data<br>anterior à edição da <b>Resolução\nCOFECI nº 1.555/2025</b>, publicada em <b>03 de outubro de<br>2025</b>.<br>Importante destacar que, as\ndisposições estabelecidas pela referida Resolução não produzem<br>efeitos sobre\nsituações ocorridas antes de sua vigência. Assim, como a norma não prevê a<br>concessão de isenção para casos anteriores à sua publicação, por conta disso <b>não\né possível<br>o deferimento do seu pedido.<br></b><br>Agradecemos a compreensão e permanecemos à disposição para\neventuais esclarecimentos.</p>"
+  },
+  {
+    "titulo": "22 Gravidez indeferido por tempo",
+    "texto": "Olá, após análise do requerimento de concessão do benefício de\nisenção de anuidade, verificou-se que o pedido de isenção <b>não atende aos\nrequisitos previstos</b> no art. 2º, incisos I e II da&nbsp;<b>Resolução COFECI nº\n1.555/2025</b>, especialmente quanto ao <b>prazo máximo de 120 (cento e vinte) dias\npara protocolo do requerimento</b>. Dessa forma, nos termos da norma vigente, o pedido fica<b> indeferido por descumprimento das exigências estabelecidas.</b><br><br>Permanecemos à disposição para esclarecimento de eventuais\ndúvidas.<div>Tenha um ótimo dia.</div>"
+  },
+  {
+    "titulo": "23 Envio de protocolo Gravidez/Adoção exercício futuro",
+    "texto": "<p class=\"MsoNormal\">Prezado(a),<br>O CRECI-SC agradece o seu contato.<br>Informamos que seu pedido encontra-se <b>pré-deferido</b>.\nConsiderando que a solicitação<br>refere-se ao <b>exercício futuro</b>,\nesclarecemos que o boleto da anuidade será emitido e<br>encaminhado ao seu e-mail,\nconforme procedimento habitual.<br>Contudo, <b><u>ressaltamos que o referido boleto não deverá\nser quitado</u></b>, pois o cancelamento<br>dele será realizado até o dia <b>31/03</b>.\nCaso o pagamento seja efetuado antes desse prazo, <b><u>a<br>Resolução vigente\nprevê que o valor não será reembolsado</u></b>, e <b>o requerimento não<br>poderá\nser refeito</b>.<br><br>Após a conclusão do procedimento iremos informar você via e-mail.</p>"
+  },
+  {
+    "titulo": "24 Envio de protocolo Gravidez/Adoção exercício atual",
+    "texto": "<p class=\"MsoNormal\">Prezado(a),<br>O CRECI-SC agradece o seu contato.<br>Informamos que seu pedido encontra-se <b>pré-deferido</b>.\nConsiderando que a solicitação<br>refere-se ao <b>exercício atual</b>,\nesclarecemos que o boleto da anuidade será emitido e<br>encaminhado ao seu e-mail,\nconforme procedimento habitual.<br>Contudo, <b><u>ressaltamos que o referido boleto não deverá\nser quitado</u></b>, pois o cancelamento<br>dele será realizado até o dia <b>31/03</b>.\nCaso o pagamento seja efetuado antes desse prazo, <b><u>a<br>Resolução vigente\nprevê que o valor não será reembolsado</u></b>, e <b>o requerimento não<br>poderá\nser refeito</b>.<br><br>Após a conclusão do procedimento iremos informar você via e-mail.</p>\n\n<p class=\"MsoNormal\"><o:p>&nbsp;</o:p></p>"
+  },
+  {
+    "titulo": "25 E-mail informando sobre o deferimento Gravidez/Adoção",
+    "texto": "<p class=\"MsoNormal\">TITULO: CRECI-SC PROTOCOLO Nº<br>Boa tarde, em atenção ao protocolo nº ____ , informamos que a sua\nsolicitação de <b>isenção referente ao exercício de ____</b> foi <b>deferida</b>,\nnos termos da <b>Resolução COFECI nº 1.555/2025</b>.<br><br>Permanecemos à disposição para quaisquer esclarecimentos\nadicionais.<br><br>Atenciosamente,<br>CRECI-SC – Contas a Receber</p>"
+  }
+];
+var atalhosIniciais = [
+  {
+    "nome": "E-MAIL",
+    "url": "https://mail.creci-sc.gov.br/"
+  },
+  {
+    "nome": "CONSELHO NET",
+    "url": "https://www.crecisc.conselho.net.br/admin_principal.php?tab=4&tab_sefin=4"
+  },
+  {
+    "nome": "PORTAL DE ATENDIMENTO",
+    "url": "https://atendimento.crecisc.conselho.net.br/#/"
+  },
+  {
+    "nome": "TABELA DE TAXAS 2026",
+    "url": "file://arquivos/setores$/financeiro/%23EDER/SISTEMA%20DE%20FRASES%20PADROES/TABELA%20DE%20TAXAS%202026.pdf"
+  },
+  {
+    "nome": "RESOLUÇÕES UTEIS",
+    "url": "file://arquivos/setores$/financeiro/%23EDER/SISTEMA%20DE%20FRASES%20PADROES/RESOLUCOES%20UTEIS.pdf"
+  }
+];
+var tiposProcessoSalvos = [
+  {
+    "nome": "REEMBOLSO",
+    "andamentos": [
+      {
+        "titulo": "DESPACHO TESOUREIRO",
+        "texto": "Após analise encaminho o protocolo para preparo de documentação e posterior despacho do Diretor Tesoureiro."
+      }
+    ]
+  }
+];
+var senhaSalva = "4321";
